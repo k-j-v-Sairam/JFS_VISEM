dd509116b37e3d6c75758f9c38e8de6e6b9e29cf
k-j-v-Sairam <kjayavenkatasairam@gmail.com>
2025-12-29 10:42:13 +0530

    Add files via upload

---
+++ ReverseNumber/src/main/java/com/skillnext/Reverse.java
@@ -0,0 +1,17 @@
+package com.skillnext;
+
+public class Reverse {
+    public static void main(String[] args) {
+
+        int number = 12345;   
+        int reversed = 0;
+
+        while (number != 0) {
+            int digit = number % 10;      
+            reversed = reversed * 10 + digit; 
+            number = number / 10; 
+        }
+
+        System.out.println("Reversed Number: " + reversed);
+    }
+}
